394541a8880ec5fa0f5aa9f61b4419c91551f312
aschulhofer <andreas.schulhofer@gmail.com>
2016-08-02 23:25:27 +0200

    Added build configuration json file; read build.conf file in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    buildConfig: grunt.file.readJSON('build.conf.json'),
 
     clean: {
       dist: {
@@ -11,28 +12,12 @@ module.exports = function(grunt) {
 
     concat: {
       js: {
-        src: [
-          'bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js',
-          'bower_components/angular/angular.js',
-          'bower_components/angular-route/angular-route.js',
-          'bower_components/angular-jwt/dist/angular-jwt.js',
-          'app/app.module.js',
-          'app/configuration/env.js',
-          'app/app.config.js',
-          'app/{components,directives,layout,services}/**/*module.js',
-          'app/{components,directives,layout,services}/**/!(*.module).js'
-        ],
-        dest: 'app/dist/build.js'
+        src: '<%= buildConfig.js.src %>',
+        dest: '<%= buildConfig.js.dest %>'
       },
       css: {
-        src: [
-            'bower_components/bootstrap/dist/css/bootstrap.css',
-            'bower_components/html5-boilerplate/dist/css/normalize.css',
-            'bower_components/html5-boilerplate/dist/css/main.css',
-            'app/app.css',
-            'app/{components,directives,layout,services}/**/*.css'
-        ],
-        dest: 'app/dist/build.css'
+        src: '<%= buildConfig.css.src %>',
+        dest: '<%= buildConfig.css.dest %>'
       }
     }
 
