9e1c40ce471546695b38ae91dff89ad64b080394
aschulhofer <andreas.schulhofer@gmail.com>
2016-08-02 23:44:14 +0200

    Updated npm task to create dist folder; updated glop-concat.js script to read build.conf.js file for files to concat

--- scripts/glob-concat.js
+++ scripts/glob-concat.js
@@ -1,6 +1,7 @@
 //jshint strict: false
 var fs = require('fs');
 var glob = require('glob');
+var loadJsonFile = require('load-json-file');
 
 function concat(options) {
     var fileList = options.src;
@@ -12,7 +13,7 @@ function concat(options) {
 
     fileList = Array.prototype.concat.apply([], fileList);
 
-    console.log(fileList);
+    console.log(fileList, distPath);
 
     var output = fileList.map(function(filePath) {
         return fs.readFileSync(filePath).toString();
@@ -20,29 +21,16 @@ function concat(options) {
     fs.writeFileSync(distPath, output.join('\n'));
 }
 
-// Concat JS files
-concat({
-    src: [
-        'bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js',
-        'bower_components/angular/angular.js',
-        'bower_components/angular-route/angular-route.js',
-        'bower_components/angular-jwt/dist/angular-jwt.js',
-        'app/app.module.js',
-        'app/configuration/env.js',
-        'app/app.config.js',
-        'app/{components,directives,layout,services}/**/*module.js',
-        'app/{components,directives,layout,services}/**/!(*.module).js'
-    ],
-    dest: 'app/dist/build.js'
-});
+loadJsonFile('build.conf.json').then(function(buildConfig) {
+  // Concat JS files
+  concat({
+      src: buildConfig.js.src,
+      dest: buildConfig.js.dest,
+  });
 
-// Concat CSS files
-concat({
-    src: [
-        'bower_components/bootstrap/dist/css/bootstrap.css',
-        'bower_components/html5-boilerplate/dist/css/normalize.css',
-        'bower_components/html5-boilerplate/dist/css/main.css',
-        'app/app.css'
-    ],
-    dest: 'app/dist/build.css'
+  // Concat CSS files
+  concat({
+      src: buildConfig.css.src,
+      dest: buildConfig.css.dest,
+  });
 });
