4ef0c4b9377ae22cac549a45b3144b94c80c7e41
aschulhofer <andreas.schulhofer@gmail.com>
2016-08-03 23:14:44 +0200

    Added watch grunt plugin; watch js and css changes and run concat tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,28 @@ module.exports = function(grunt) {
         src: '<%= buildConfig.css.src %>',
         dest: '<%= buildConfig.css.dest %>'
       }
+    },
+
+    watch: {
+      options: {
+        debounceDelay: 1500
+      },
+      js: {
+        files: ['!app/dist/*', 'app/**/*.js'],
+        tasks: ['concat:js']
+      },
+      css: {
+        files: ['!app/dist/*', 'app/**/*.css'],
+        tasks: ['concat:css']
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('dist', ['clean:dist', 'concat:js', 'concat:css']);
+  grunt.registerTask('run', ['dist', 'watch']);
 };
